ee3edab4d6afbe26cac75baaec0c42836254e397
aufula <aufula@hotmail.com>
2012-08-01 08:48:48 +0800

    update

--- get.sh
+++ get.sh
@@ -17,11 +17,11 @@ cd templates/$module/
 
 for x in *.html
 do
-    wget --restrict-file-names=unix http://0.0.0.0:8888/$module/$x -O $dst/$x
+    wget --restrict-file-names=unix http://0.0.0.0:8888/$module/$x?usergroup=$2 -O $dst/$x
     # echo $x
 done
 
-cd $dst
-cd ..
-tar -zcvf $module$(date +%Y%m%d_%H%M%S).tar.gz $module
+# cd $dst
+# cd ..
+# tar -zcvf $module$(date +%Y%m%d_%H%M%S).tar.gz $module
 
