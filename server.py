9ccee701cf4aff0528e0b546add5ef5f2c8036c3
aufula <aufula@hotmail.com>
2012-08-03 17:35:22 +0800

    guonei

--- server.py
+++ server.py
@@ -11,10 +11,10 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
-            (r"/guoji/(?<!ajax)([^/]+).html", GuojiHandler),
-            (r"/guoji/ajax/(.+)", GuojiAjaxHandler),
-            (r"/guonei/(?<!ajax)([^/]+).html", GuoneiHandler),
-            (r"/guonei/ajax/(.+)", GuoneiAjaxHandler),
+            (r"/(guoji)/(?<!ajax)([^/]+).html", GuojiHandler),
+            (r"/(guoji)/ajax/(.+)", GuojiAjaxHandler),
+            (r"/(guonei)/(?<!ajax)([^/]+).html", GuoneiHandler),
+            (r"/(guonei)/ajax/(.+)", GuoneiAjaxHandler),
         ]
         settings = dict(
             debug='yes',
@@ -31,7 +31,7 @@ class Application(tornado.web.Application):
 
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self, pagename = 'index'):
+    def get(self, section, pagename = 'index'):
         data = {
             'title':pagename,
         }
@@ -39,7 +39,7 @@ class MainHandler(tornado.web.RequestHandler):
 
 
 class GuoneiHandler(tornado.web.RequestHandler):
-    def get(self, pagename = 'index'):
+    def get(self, section, pagename = 'index'):
         usergroup = self.get_argument('usergroup',default='cgs')
         if(usergroup=='yys'):
             data = {
@@ -107,13 +107,14 @@ class GuoneiHandler(tornado.web.RequestHandler):
 
         data['parent_title']='国内机票'
         data['title']=pagename
+        data['section']=section
 
         template = 'guonei/'+pagename.encode('utf-8')+'.html'
         self.render(template, data = data, usergroup = usergroup)
 
 
 class GuoneiAjaxHandler(GuoneiHandler):
-    def get(self, pagename = 'index'):
+    def get(self, section, pagename = 'index'):
         template = 'guonei/ajax/'+pagename.encode('utf-8')
         self.render(template)
 
@@ -121,37 +122,38 @@ class GuoneiAjaxHandler(GuoneiHandler):
 #GUOJI
 #######################
 class GuojiHandler(tornado.web.RequestHandler):
-    def get(self, pagename = 'index'):
+    def get(self, section, pagename = 'index'):
         usergroup = self.get_argument('usergroup',default='cgs')
         if(usergroup=='yys'):
             data = {
-                'parent_title':'国际机票',
                 'usergroup':'运营商',
                 'usergroup-id':'yys',
-                'title':pagename,
                 'sidebar':[
-                    [u'机票采购',
-                     [u'PNR预订',u'白屏预订']
+                    [u'机票采购', [
+                        u'PNR预订',u'白屏预订']
                     ],
-                    [u'国际订单管理',
-                     [u'出票订单',u'改签订单',u'退票订单',u'废票订单',u'退款订单']
+                    [u'国际订单管理', [
+                        u'出票订单',u'改签订单',u'退票订单',u'废票订单',u'退款订单']
+                    ],
+                    [u'机票出票管理', [u'机票出票管理']
+                    ],
+                    [u'国际政策管理',
+                        [u'国际返点查询',u'新增国际返点',u'国际政策导入'] 
                     ],
                     [u'发票信息管理',[
                         u'发票信息查询',
-                        u'入库信息管理',
-                    ]],
+                        u'入库信息管理', ]
+                    ],
                     [u'基础数据维护',[
-                        u'基础数据维护',
-                    ]],
+                        u'基础数据维护', ]
+                    ],
                 ],
             }
 
         if (usergroup=='gys'):
             data = {
-                'parent_title':'国际机票',
                 'usergroup':'供应商',
                 'usergroup-id':'gys',
-                'title':pagename,
                 'sidebar':[
                     [u'国际订单管理',
                      [u'出票订单',u'审核订单',u'退票订单',u'废票订单']
@@ -170,10 +172,8 @@ class GuojiHandler(tornado.web.RequestHandler):
 
         if (usergroup=='cgs'):
             data = {
-                'parent_title':'国际机票',
                 'usergroup':'采购商',
                 'usergroup-id':'cgs',
-                'title':pagename,
                 'sidebar':[
                     [u'机票采购',
                      [u'PNR预订',u'白屏预订']
@@ -193,6 +193,10 @@ class GuojiHandler(tornado.web.RequestHandler):
                 ],
             }
 
+        data['parent_title']='国际机票'
+        data['title']=pagename
+        data['section']=section
+
         template = 'guoji/'+pagename.encode('utf-8')+'.html'
         self.render(template, data = data, usergroup = usergroup)
 
@@ -202,7 +206,7 @@ class GuojiHandler(tornado.web.RequestHandler):
 
 
 class GuojiAjaxHandler(GuojiHandler):
-    def get(self, pagename = 'index'):
+    def get(self, section, pagename = 'index'):
         template = 'guoji/ajax/'+pagename.encode('utf-8')
         self.render(template)
 
